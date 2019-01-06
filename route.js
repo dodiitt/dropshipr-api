81ec0ebd86df0e33c0bbecd41acddc79d7dff07a
radityasurya <radityasurya@hotmail.com>
2019-01-06 16:01:14 +0100

    Register email route to the app routes

--- route.js
+++ route.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const emailRoutes = require("./services/email/email.route");
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 /** GET /healthz - Check service health */
-router.get("/healthz", (req, res) => res.send("OK"));
+router.get("/healthz", (req, res) =>
+  res.json({
+    code: res.statusCode,
+    message: "Healthy"
+  })
+);
+
+router.use("/emails", emailRoutes);
 
 module.exports = router;
